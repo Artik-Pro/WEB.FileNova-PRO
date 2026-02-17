ce40d7ee509cb021d14126e41d6939cbf3d1912c
Artik-Pro <mufikfilms@gmail.com>
2026-02-17 18:23:52 +0200

    Create config.php file with version, constants, and plugin settings

---
+++ includes/config.php
@@ -0,0 +1,21 @@
+<?php
+// Configuration file for WEB.FileNova-PRO
+
+// Current Date and Time
+define('CURRENT_DATE_TIME', '2026-02-17 16:22:30');
+
+// User Login
+define('USER_LOGIN', 'Artik-Pro');
+
+// Version
+define('VERSION', '1.0.0');
+
+// Constants
+define('PLUGIN_PATH', '/path/to/plugins');
+
+// Plugin Settings
+$plugin_settings = array(
+    'plugin_name' => 'ExamplePlugin',
+    'enabled' => true,
+    'version' => '1.0.0',
+);
